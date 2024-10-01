f04884a57887069b9dcdd6565c8fc2677af4ac24
Bendik Solheim <hello@bendik.dev>
2024-10-01 21:46:25 +0200

    Change gui framework to iced

--- src/main.rs
+++ src/main.rs
@@ -1,232 +1,115 @@
-use gpui::*;
-use libc::TIOCSCTTY;
-use std::fs::File;
-use std::io::{self, Error, ErrorKind, Read, Write};
-use std::os::fd::{AsRawFd, OwnedFd};
-use std::os::unix::process::CommandExt;
-use std::process::{Child, Command};
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
+mod term;
 
-struct Content {
-    content: Arc<Mutex<Vec<Vec<u8>>>>,
+use iced::{
+    futures::{
+        channel::mpsc::{self},
+        SinkExt,
+    },
+    keyboard::{self, Key, Modifiers},
+    widget::{column, text, Column},
+    Color, Font, Subscription, Task,
+};
+use rustix_openpty::rustix::termios::Winsize;
+use term::Output;
+
+fn main() -> iced::Result {
+    iced::application("Terminal", Terminalview::update, Terminalview::view)
+        .decorations(false)
+        .subscription(Terminalview::subscription)
+        .run_with(Terminalview::new)
 }
 
-impl Content {
-    fn new() -> Self {
-        Self {
-            content: Arc::new(Mutex::new(vec![vec![]])),
-        }
+struct Terminalview {
+    width: u16,
+    height: u16,
+    content: String,
+    sender: Option<mpsc::Sender<term::TermMessage>>,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    TerminalOutput(String),
+    TerminalInput,
+    Keyboard(Key, Modifiers),
+    Term(term::Event),
+}
+
+impl Terminalview {
+    fn new() -> (Self, Task<Message>) {
+        let model = Self {
+            width: 80,
+            height: 24,
+            content: String::new(),
+            sender: None,
+        };
+
+        (model, Task::none())
     }
-}
 
-struct TerminalView {
-    master: File,
-    output: Model<Content>,
-    strip_ansi: bool,
-    focus_handle: FocusHandle,
-}
+    fn view(&self) -> Column<Message> {
+        column![text(self.content.clone())
+            .font(Font::MONOSPACE)
+            .color(Color::from_rgb8(0xff, 0xff, 0xff))
+            .size(14),]
+    }
 
-struct Pty {
-    master: OwnedFd,
-    slave: OwnedFd,
-}
-
-fn open_pty() -> Result<Pty, Error> {
-    // Ask OS for a PTY
-    let pty = rustix_openpty::openpty(None, None)?;
-
-    // Make reads on master non-blocking
-    unsafe { libc::fcntl(pty.controller.as_raw_fd(), libc::F_SETFL, libc::O_NONBLOCK) };
-
-    // Return a struct with master and slave side of PTY
-    Ok(Pty {
-        master: pty.controller,
-        slave: pty.user,
-    })
-}
-
-fn spawn_shell(slave: &OwnedFd, shell: &str) -> io::Result<Child> {
-    let mut command = Command::new(shell);
-    command.env("TERM", "dumb");
-    command.stdin(slave.try_clone()?);
-    command.stdout(slave.try_clone()?);
-    command.stderr(slave.try_clone()?);
-
-    let slave_fd = slave.as_raw_fd();
-    unsafe {
-        command.pre_exec(move || {
-            // Become leader of new session
-            let err = libc::setsid();
-            if err == -1 {
-                return Err(Error::new(io::ErrorKind::Other, "Failed to set session ID"));
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {
+            Message::TerminalOutput(s) => {
+                self.content.push_str(&s);
+                Task::none()
             }
-
-            // Set controlling terminal
-            let res = libc::ioctl(slave_fd, TIOCSCTTY as _, 0);
-            if res < 0 {
-                return Err(Error::new(io::ErrorKind::Other, "Failed to set controlling terminal"));
-            }
-
-            libc::close(slave_fd);
-
-            Ok(())
-        });
-    }
-
-    command.spawn()
-}
-
-fn read_output(master: &OwnedFd, cx: &mut ViewContext<TerminalView>) -> Model<Content> {
-    let content_model = cx.new_model(|_| Content::new());
-    let content = content_model.clone();
-    let master_clone = master.try_clone().unwrap();
-
-    cx.spawn(|_, mut cx| async move {
-        let mut buffer = [0u8; 1024];
-        let mut file = File::from(master_clone);
-        loop {
-            async_std::task::sleep(Duration::from_millis(16)).await;
-            match file.read(&mut buffer) {
-                Ok(0) => break,
-                Ok(num_bytes) => {
-                    cx.update_model(&content, |model, cx| {
-                        let mut _content = model.content.lock().unwrap();
-                        let bytes = buffer[..num_bytes].to_vec();
-                        if bytes == b"\n" {
-                            // Newline
-                            _content.push(vec![]);
-                        } else if bytes == b"\x08 \x08" {
-                            // Backspace
-                            if let Some(line) = _content.last_mut() {
-                                if line.len() > 0 {
-                                    line.remove(line.len() - 1);
-                                }
-                            }
-                        } else {
-                            // All the rest
-                            if let Some(line) = _content.last_mut() {
-                                line.extend(bytes);
-                            }
-                        }
-                        drop(_content);
-                        cx.notify();
-                    })
-                    .unwrap();
-                }
-                Err(e) => {
-                    // WouldBlock is expected when there is no input
-                    if e.kind() != ErrorKind::WouldBlock {
-                        eprint!("Error reading from PTY: {:?}", e)
-                    }
-                }
-            }
-        }
-    })
-    .detach();
-
-    content_model
-}
-
-impl TerminalView {
-    fn new(cx: &mut ViewContext<Self>) -> Result<Self, Error> {
-        // Retrieve shell from env var
-        let shell = std::env::var("SHELL").expect("Expected to find default shell in $SHELL env var");
-
-        // Ask OS for a PTY
-        let pty = open_pty()?;
-
-        // Listen for input to master
-        let output = read_output(&pty.master.try_clone().unwrap(), cx);
-
-        // Observe changes in the model and notify gpui
-        cx.observe(&output, |_, _, cx| {
-            cx.notify();
-        })
-        .detach();
-
-        // Spawn shell and connect to slave side of PTY
-        let result = spawn_shell(&pty.slave, shell.as_str());
-
-        // Return model if shell spawns without error
-        result
-            .map(|_| Self {
-                master: File::from(pty.master.try_clone().unwrap()),
-                output,
-                strip_ansi: true,
-                focus_handle: cx.focus_handle(),
-            })
-            .map_err(|err| Error::new(err.kind(), format!("Failed to spawn command '{}': {}", shell, err)))
-    }
-
-    fn send_input(&self, input: &str) {
-        let mut f = self.master.try_clone().unwrap();
-        let _ = f.write_all(input.as_bytes());
-        let _ = f.flush();
-    }
-
-    fn toggle_ansi(&mut self) {
-        self.strip_ansi = !self.strip_ansi;
-    }
-}
-
-impl Render for TerminalView {
-    fn render(&mut self, cx: &mut ViewContext<TerminalView>) -> impl IntoElement {
-        cx.focus(&self.focus_handle);
-        let content = self.output.read(cx).content.lock().unwrap();
-
-        div()
-            .pt_5()
-            .track_focus(&self.focus_handle)
-            .on_key_down(
-                cx.listener(|this, event: &KeyDownEvent, _cx| match event.keystroke.key.as_str() {
-                    "enter" => this.send_input("\r"),
-                    "backspace" => this.send_input("\x7f"),
-                    "space" => this.send_input(" "),
-                    "t" => {
-                        if event.keystroke.modifiers.control {
-                            this.toggle_ansi();
-                            _cx.notify();
-                        } else {
-                            this.send_input("t");
-                        }
-                    }
-                    key if key.len() == 1 => this.send_input(key),
-                    _ => {}
-                }),
-            )
-            .size_full()
-            .bg(rgb(0x282c34))
-            .text_color(rgb(0xabb2bf))
-            .font(font("monospace"))
-            .child(div().size_full().p_2().items_start().child(div().flex_col().children(
-                content.iter().rev().take(34).rev().map(|bytes| {
-                    let line = match self.strip_ansi {
-                        true => String::from_utf8(strip_ansi_escapes::strip(bytes)).unwrap(),
-                        false => String::from_utf8_lossy(bytes).to_string(),
+            Message::Keyboard(k, _) => {
+                if let Some(sender) = self.sender.clone() {
+                    let f = async move {
+                        let mut sender = sender;
+                        sender
+                            .send(term::TermMessage::Input(k))
+                            .await
+                            .expect("Could not send TermMessage::Input");
                     };
-                    div().child(format!("{}", line))
-                }),
-            )))
+                    Task::perform(f, |_| Message::TerminalInput)
+                } else {
+                    Task::none()
+                }
+            }
+            Message::TerminalInput => Task::none(),
+            Message::Term(term_event) => match term_event {
+                term::Event::Ready(sender) => {
+                    self.sender = Some(sender);
+                    Task::none()
+                }
+                term::Event::Output(s) => {
+                    match s {
+                        Output::Text(s) => {
+                            self.content.push_str(&s);
+                        }
+                        Output::Newline => {
+                            self.content.push('\n');
+                        }
+                        Output::Backspace => {
+                            self.content.remove(1);
+                        }
+                    }
+                    Task::none()
+                }
+            },
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        let keypress = keyboard::on_key_press(|key, modifier| Some(Message::Keyboard(key, modifier)));
+        let winsize = create_winsize(self.width, self.height);
+        let term_sub = Subscription::run_with_id(12345, term::Term::spawn(winsize)).map(Message::Term);
+        iced::Subscription::batch([term_sub, keypress])
     }
 }
 
-actions!(term, [Quit]);
-
-fn main() {
-    let window_options = WindowOptions {
-        titlebar: Some(TitlebarOptions {
-            appears_transparent: true,
-            ..Default::default()
-        }),
-        ..Default::default()
-    };
-    App::new().run(|cx: &mut AppContext| {
-        cx.activate(true);
-        cx.bind_keys([KeyBinding::new("cmd-q", Quit, None)]);
-        cx.on_action(|_: &Quit, cx| cx.quit());
-
-        cx.open_window(window_options, |cx| cx.new_view(|cx| TerminalView::new(cx).unwrap()))
-            .unwrap();
-    });
+fn create_winsize(width: u16, height: u16) -> Winsize {
+    Winsize {
+        ws_col: width,
+        ws_row: height,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    }
 }
---
+++ src/term.rs
@@ -0,0 +1,164 @@
+use iced::{
+    futures::{
+        channel::mpsc::{self, Sender},
+        SinkExt, Stream, StreamExt,
+    },
+    keyboard::Key,
+};
+use libc::TIOCSCTTY;
+use rustix_openpty::rustix::termios::Winsize;
+use std::{
+    fs::File,
+    io::{self, Error, ErrorKind, Read, Write},
+    os::{
+        fd::{AsRawFd, OwnedFd},
+        unix::process::CommandExt,
+    },
+    process::{Child, Command},
+    time::Duration,
+};
+
+pub struct Term {}
+
+impl Term {
+    pub fn spawn(winsize: Winsize) -> impl Stream<Item = Event> {
+        let pty = open_pty(winsize).expect("Could not get PTY");
+        let master = File::from(pty.master.try_clone().unwrap());
+
+        iced::stream::channel(100, |mut output| async move {
+            let shell = std::env::var("SHELL").expect("$SHELL is not set");
+            read_output(&pty.master, output.clone());
+            let _ = spawn_shell(&pty.slave, shell.as_str());
+            let (sender, mut receiver) = mpsc::channel(100);
+            output
+                .send(Event::Ready(sender))
+                .await
+                .expect("Could not send Message::Ready");
+
+            loop {
+                let input = receiver.select_next_some().await;
+                match input {
+                    TermMessage::Input(input) => match input {
+                        Key::Named(named) => match named {
+                            iced::keyboard::key::Named::Enter => send(&master, "\r"),
+                            iced::keyboard::key::Named::Space => send(&master, " "),
+                            iced::keyboard::key::Named::Backspace => send(&master, "\x7f"),
+                            _ => {}
+                        },
+                        Key::Character(c) => {
+                            send(&master, c.as_str());
+                        }
+                        Key::Unidentified => todo!(),
+                    },
+                }
+            }
+        })
+    }
+}
+
+fn send(mut master: &File, content: &str) {
+    master.write_all(content.as_bytes()).unwrap();
+    master.flush().unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub enum Event {
+    Ready(mpsc::Sender<TermMessage>),
+    Output(Output),
+}
+
+#[derive(Debug, Clone)]
+pub enum Output {
+    Text(String),
+    Newline,
+    Backspace,
+}
+
+#[derive(Debug, Clone)]
+pub enum TermMessage {
+    Input(Key),
+}
+
+struct Pty {
+    master: OwnedFd,
+    slave: OwnedFd,
+}
+
+fn open_pty(winsize: Winsize) -> Result<Pty, Error> {
+    // Ask OS for a PTY
+    let pty = rustix_openpty::openpty(None, Some(&winsize))?;
+
+    // Make reads on master non-blocking
+    unsafe { libc::fcntl(pty.controller.as_raw_fd(), libc::F_SETFL, libc::O_NONBLOCK) };
+
+    // Return a struct with master and slave side of PTY
+    Ok(Pty {
+        master: pty.controller,
+        slave: pty.user,
+    })
+}
+
+fn read_output(master: &OwnedFd, mut sender: Sender<Event>) {
+    let master_clone = master.try_clone().unwrap();
+
+    let _a = async_std::task::spawn(async move {
+        std::io::stdout().flush().unwrap();
+        let mut buffer = [0u8; 1024];
+        let mut file = File::from(master_clone);
+        loop {
+            async_std::task::sleep(Duration::from_millis(16)).await;
+            match file.read(&mut buffer) {
+                Ok(0) => break,
+                Ok(num_bytes) => {
+                    let bytes = buffer[..num_bytes].to_vec();
+                    if bytes == b"\n" {
+                        sender.send(Event::Output(Output::Newline)).await.unwrap();
+                    } else if bytes == b"\x08 \x08" {
+                        sender.send(Event::Output(Output::Backspace)).await.unwrap();
+                    } else {
+                        if let Ok(str) = String::from_utf8(bytes) {
+                            sender.send(Event::Output(Output::Text(str))).await.unwrap();
+                        }
+                    }
+                }
+                Err(e) => {
+                    // WouldBlock is expected when there is no input
+                    if e.kind() != ErrorKind::WouldBlock {
+                        eprint!("Error reading from PTY: {:?}", e)
+                    }
+                }
+            }
+        }
+    });
+}
+
+fn spawn_shell(slave: &OwnedFd, shell: &str) -> io::Result<Child> {
+    let mut command = Command::new(shell);
+    command.env("TERM", "dumb");
+    command.stdin(slave.try_clone()?);
+    command.stdout(slave.try_clone()?);
+    command.stderr(slave.try_clone()?);
+
+    let slave_fd = slave.as_raw_fd();
+    unsafe {
+        command.pre_exec(move || {
+            // Become leader of new session
+            let err = libc::setsid();
+            if err == -1 {
+                return Err(Error::new(io::ErrorKind::Other, "Failed to set session ID"));
+            }
+
+            // Set controlling terminal
+            let res = libc::ioctl(slave_fd, TIOCSCTTY as _, 0);
+            if res < 0 {
+                return Err(Error::new(io::ErrorKind::Other, "Failed to set controlling terminal"));
+            }
+
+            libc::close(slave_fd);
+
+            Ok(())
+        });
+    }
+
+    command.spawn()
+}
